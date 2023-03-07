0f1c966e5633c41f95c40441622e37075308604c
zinb9419 <zinb9419@gmail.com>
2023-03-07 13:04:40 +0100

    cpp07

---
+++ ex00/whatever.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 13:02:01 by ziloughm          #+#    #+#             */
+/*   Updated: 2023/03/07 13:04:12 by ziloughm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+# endif
